3340d199e3bafd9370d7f977df88f3df19f24727
isgardel <isaacgarcia@iMac-de-Isaac-Garcia.local>
2015-06-10 12:24:06 +0100

    Primera página y favicon

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {  //se ejecutará sólo para GET
+  res.render('index', { title: 'Quiz' }); //renderizamos la vista a index, que es /views/index.ejs
 });
 
 module.exports = router;
