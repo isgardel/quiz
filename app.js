49b427a8df074adb46a9ac66ae676ab193ab4a91
isgardel <isaacgarcia@iMac-de-Isaac-Garcia.local>
2015-06-15 11:13:48 +0100

    express partials

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
-//var users = require('./routes/users'); //eliminanmos el acceso a users
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); //elegimos este motor de vistas, no el por defecto de express
+app.set('view engine', 'ejs');
 
+app.use(partials());
 // uncomment after placing your favicon in /public
+// app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,8 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes); //este router, se ejecutará siempre, para GET, POST, PUT y DELETE
-//app.use('/users', users); //eliminanmos el acceso a users
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controlles/quiz_controller');
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {  //se ejecutará sólo para GET
-  res.render('index', { title: 'Quiz' }); //renderizamos la vista a index, que es /views/index.ejs
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
 });
 
-router.get('quizes/question', quizController.question);
-router.get('quizes/answer', quizController.answer);
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
+
 
 module.exports = router;
