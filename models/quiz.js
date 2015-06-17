d5ffbfce234f0bf2a6b91778fbf8206d45f104a6
isgardel <isaacgarcia@iMac-de-Isaac-Garcia.local>
2015-06-17 10:52:47 +0100

    Despliegue DB en Heroku

--- models/quiz.js
+++ models/quiz.js
@@ -3,6 +3,6 @@
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('Quiz',
                             { pregunta: DataTypes.STRING,
-                              respuesta: DataTypes.STRING,
+                              respuesta: DataTypes.STRING
                               });
 }
